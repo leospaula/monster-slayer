376ac2ae1be438c4efe7385e17edd28e765860a9
Leo <leo.spaula@gmail.com>
2019-10-01 18:13:24 -0300

    Add Trophy seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,28 @@ ActiveRecord::Base.establish_connection(db_configuration["development"])
 Model::Monster.find_or_create_by(name: 'Imp')
 Model::Monster.find_or_create_by(name: 'Winged Imp')
 
-p "Monsters : #{Model::Monster.all.inspect}"
+Model::Trophy.find_or_create_by(name: 'Bronze', image: 'bronze_trophy.png')
+Model::Trophy.find_or_create_by(name: 'Silver', image: 'silver_trophy.png')
+Model::Trophy.find_or_create_by(name: 'Gold', image: 'gold_trophy.png')
+Model::Trophy.find_or_create_by(name: 'Platinum', image: 'platinum_trophy.png')
+Model::Trophy.find_or_create_by(name: 'Diamond', image: 'diamond_trophy.png')
+
+goal_values = {
+  'monster': { desc: 'Monstros', init: 10, step: 5 },
+  'coin': { desc: 'Moedas', init: 100, step: 100 },
+  'death': { desc: 'Mortes', init: 2, step: 3 }
+}
+
+goal_values.each do |key, value|
+  goal_value = value[:init]
+  Model::Trophy.all.each do |trophy|
+    Model::Goal.find_or_create_by(
+      trophy_id: trophy.id,
+      description: "#{goal_value} #{value[:desc]}",
+      category: key,
+      value: goal_value
+    )
+
+    goal_value += value[:step]
+  end
+end
